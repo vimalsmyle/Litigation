3477e519d1b1d11a1620668a2c8e324d43f682ff
DELL <DELL@DESKTOP-V5ETALU>
2021-05-12 17:13:19 +0530

    controller

---
+++ src/main/java/com/mlj/legalaffairs/Litigation/controller/AddController.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.mlj.legalaffairs.Litigation.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author K Vimal Kumar
+ *
+ */
+@Controller
+public class AddController {
+
+}
